3450c62df6a2d5ad72cf7b66f7edd30ac863199d
phoenix synkova <synkova.work@gmail.com>
2020-06-06 15:51:37 -0700

    added some comments and revised camera movement script

--- practice_2d/Assets/Scripts/CameraFollow.cs
+++ practice_2d/Assets/Scripts/CameraFollow.cs
@@ -1,12 +1,28 @@
 ﻿using UnityEngine;
 
+
 public class CameraFollow : MonoBehaviour
 {
-  public Transform target;
-  public float smoothSpeed = 0.125f;
-  public Vector3 offset;
+  private Transform target;
+  private Vector3 offset = new Vector3(0, 0, -10);
+
+  void Start() {
+    GetPlayerTransform();
+  }
 
   void LateUpdate () {
     transform.position = target.position + offset;
+  }
+
+  // Get the Player game object by Tag, if possible:
+  // update the target transform information
+  // otherwise, log an error message
+  void GetPlayerTransform () {
+    GameObject player = GameObject.FindWithTag("Player");
+    if (player != null) {
+      target = player.transform;
+    } else {
+      Debug.Log("Player object not found");
+    }
   }
 }
--- practice_2d/Assets/Scripts/PlayerController.cs
+++ practice_2d/Assets/Scripts/PlayerController.cs
@@ -26,27 +26,41 @@ public class PlayerController : MonoBehaviour
       anim.SetInteger("state", (int)state);
     }
 
+    /*
+      Performs move operations:
+      moves character right and left based on the key pressed
+      flips the character sprite accordingly
+      handles the jump making when an appropriate key is pressed
+    */
     void Move() {
       float hDir = Input.GetAxis("Horizontal");
       float speed = 450f;
       float jumpForce = 10f;
 
+      // moving left
       if (hDir < 0) {
         rb.velocity = new Vector2 (hDir * speed * Time.deltaTime, rb.velocity.y);
         gameObject.GetComponent<SpriteRenderer>().flipX = true;
       }
-
+      // moving right
       else if (hDir > 0) {
         rb.velocity = new Vector2 (hDir * speed * Time.deltaTime, rb.velocity.y);
         gameObject.GetComponent<SpriteRenderer>().flipX = false;
       }
-
+      // allowing the jump to happen only on the ground
       if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground)) {
         rb.velocity = new Vector2 (rb.velocity.x, jumpForce);
         state = State.jumping;
       }
     }
 
+    /*
+      A finite state machine that handles the character State based on the conds
+      idle -> running,
+      running -> or running -> idle
+      jumping -> falling (jumping state is triggered in Move())
+      falling -> idle
+    */
     void VelocityState() {
 
       switch(state) {
